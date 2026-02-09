4d86fd1b0571c10747a0b0f65eb1e0fee01b4fdc
Gustavo Henrique <83016025+GustavoHenrique-hub@users.noreply.github.com>
2026-02-09 16:51:47 -0300

    feat: "some updates made"

---
+++ src/main/java/com/enterprise/controller/admin/ThemeBean.java
@@ -0,0 +1,23 @@
+package com.enterprise.controller.admin;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class ThemeBean implements Serializable {
+    private boolean darkTheme = false;
+
+    public boolean isDarkTheme() {
+        return darkTheme;
+    }
+
+    public void toggleTheme() {
+        darkTheme = !darkTheme;
+    }
+
+    public String getBodyClass() {
+        return darkTheme ? "theme-dark" : "theme-light";
+    }
+}
